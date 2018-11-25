8a165beb51ee6f7011e7152a74b384c6ffa92a29
noonespecial009 <noonespecial009@gmail.com>
2018-11-24 16:57:28 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+## This assignment for CSC464 is meant to provide an introductory look at our final project which is due at then end of the semester. The instructions, credits and comments for this assignment are located in the file assign3_ipfs.pdf 
---
+++ src/storehash.js
@@ -0,0 +1,37 @@
+import web3 from './web3';
+
+//access our local copy to contract deployed on rinkeby testnet
+//use your own contract address
+const address = '0xf0Ca3Cb9718359f4079E22d2eBFBDC98f98F4206';
+//use the ABI from your contract
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+export default new web3.eth.Contract(abi, address);
